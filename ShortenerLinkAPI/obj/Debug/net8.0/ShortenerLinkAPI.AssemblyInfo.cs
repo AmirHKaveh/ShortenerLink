dfa4a38495b96d973d50318aadaaef0fa6a953a0
Amirhossein Kaveh <amirhkaveh1@gmail.com>
2024-12-07 14:08:06 +0330

    [ADD] blazor app added

--- ShortenerLinkAPI/obj/Debug/net8.0/ShortenerLinkAPI.AssemblyInfo.cs
+++ ShortenerLinkAPI/obj/Debug/net8.0/ShortenerLinkAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShortenerLinkAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70d22b2f2932ae0e5792056e07253520e30c85cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShortenerLinkAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShortenerLinkAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
