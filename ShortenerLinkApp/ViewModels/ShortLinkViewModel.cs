6d79ebc2a05567e345f3b0b8397541111b2cba59
AmirHKaveh <amirhkaveh1@gmail.com>
2024-12-12 21:54:36 +0330

    [ADD] link generator page added

---
+++ ShortenerLinkApp/ViewModels/ShortLinkViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShortenerLinkApp.ViewModels
+{
+    public class ShortLinkRequestDto
+    {
+        [DisplayName("Url")]
+        [Required(ErrorMessage = "Please Enter Your {0}")]
+        [DataType(DataType.Url, ErrorMessage = "Invalid Your {0}")]
+        public string Url { get; set; }
+    }
+}
