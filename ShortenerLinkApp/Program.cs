df2ad507a73369ff4e9923cb67c14ae6798c5b77
Amirhossein Kaveh <amirhkaveh1@gmail.com>
2024-12-14 15:05:01 +0330

    [UPD] qr code added

--- ShortenerLinkApp/Program.cs
+++ ShortenerLinkApp/Program.cs
@@ -1,7 +1,10 @@
+using Blazor.QrCodeGen;
+
 using Blazored.LocalStorage;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 
 using MudBlazor;
 using MudBlazor.Services;
@@ -19,6 +22,7 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddSingleton<OTPSharedDataService>();
 builder.Services.AddSingleton<ISnackbarService, ToastService>();
 builder.Services.AddSingleton<ISnackbar,SnackbarService>();
+builder.Services.AddTransient(sp => new ModuleCreator(sp.GetService<IJSRuntime>()));
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
